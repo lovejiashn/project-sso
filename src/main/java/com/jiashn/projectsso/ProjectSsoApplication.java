a711c55b91344d1cf9757542eed12c587e296ff8
jiangjs <421582841@qq.com>
2021-11-29 20:28:40 +0800

    [新增]添加spring-security

---
+++ src/main/java/com/jiashn/projectsso/ProjectSsoApplication.java
@@ -0,0 +1,16 @@
+package com.jiashn.projectsso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jiangjs
+ */
+@SpringBootApplication
+public class ProjectSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSsoApplication.class, args);
+    }
+
+}
